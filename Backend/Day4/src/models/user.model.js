1ea3d0f95a993a97fdcacfca49d49d6530f6e26f
nehasharmacode123 <nehasharma962595@gmail.com>
2026-01-16 17:32:49 +0530

    set bcrypt

--- Backend/Day4/src/models/user.model.js
+++ Backend/Day4/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
@@ -49,5 +51,15 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
 
 export const User = mongoose.model("User", userSchema)
